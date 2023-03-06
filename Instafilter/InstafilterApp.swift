4f872eaeb82167c35652f9c6b6b1d7a89b6da36e
Maria Romanchuk <mashulya_r_88@mail.ru>
2023-03-06 13:55:49 +0700

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Мария Романчук on 06.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
